bb6082e7dc9aa9e399b3243cb1ca9a199b77d081
Man Trinh <man.trinh@manifera.com>
2020-04-11 21:13:00 +0700

    Add eddyclo theme

--- Modules/Product/Resources/views/admin/productcategories/partials/fields.blade.php
+++ Modules/Product/Resources/views/admin/productcategories/partials/fields.blade.php
@@ -1,11 +1,16 @@
+@php
+$name = !empty($productcategory) ? $productcategory->name : old('name');
+$description = !empty($productcategory) ? $productcategory->description : old('description');
+@endphp
+
 <div class='form-group{{ $errors->has('name') ? ' has-error' : '' }}'>
     {!! Form::label('name', 'Name') !!}
-    {!! Form::text('name', old('name'), ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+    {!! Form::text('name', $name, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
     {!! $errors->first('Name', '<span class="help-block">:message</span>') !!}
 </div>
 
 <div class='form-group{{ $errors->has('description') ? ' has-error' : '' }}'>
     {!! Form::label('description', 'Description') !!}
-    {!! Form::text('description', old('description'), ['class' => 'form-control', 'placeholder' => 'Description']) !!}
+    {!! Form::text('description', $description, ['class' => 'form-control', 'placeholder' => 'Description']) !!}
     {!! $errors->first('Description', '<span class="help-block">:message</span>') !!}
 </div>
---
+++ Themes/EddyClo/webpack.mix.js
@@ -0,0 +1,3 @@
+const mix = require('laravel-mix');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const themeInfo = require('./theme.json');
