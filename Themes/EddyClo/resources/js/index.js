7d68a7863fb62748bdcc399ddc76aae00fe07124
Man Trinh <man.trinh@manifera.com>
2020-05-02 11:12:28 +0700

    Remove lang

--- Themes/EddyClo/resources/js/index.js
+++ Themes/EddyClo/resources/js/index.js
@@ -1,13 +1,13 @@
 import { render } from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
-import React, {  StrictMode } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import React, { StrictMode } from 'react';
 import App from './App'
 
 
 render(
     <StrictMode>
-        <Router basename="/en">
-            <App/>
+        <Router>
+            <App />
         </Router>
     </StrictMode>,
     document.getElementById('app')
