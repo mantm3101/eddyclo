ff4be046117711e4d0872e752139a994aeafd613
Man Trinh <man.trinh@manifera.com>
2020-09-04 08:20:53 +0700

    test

--- Modules/Core/Http/Middleware/LocaleSessionRedirectMiddleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Core\Http\Middleware;
-
-use Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect;
-
-class LocaleSessionRedirectMiddleware extends LocaleSessionRedirect
-{
-}
